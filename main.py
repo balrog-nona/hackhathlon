0255908b6eae604eec8cf7ee527b908b65145fd4
balrog <nona.h@seznam.cz>
2019-12-08 15:07:00 +0100

    url na vice dnu a kanalu

--- main.py
+++ main.py
@@ -4,12 +4,17 @@ import xml.etree.ElementTree as ET
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-response = requests.get('https://www.ceskatelevize.cz/services-old/programme/xml/schedule.php?user=test&date=08.12.2019&channel=ct24')
-response.encoding='utf-8'
-root = ET.fromstring(response.text)
+# tvorba url adres pro ruzne dny a druzne kanaly
+dates = ('08.12.2019', '09.12.2019', '10.12.2019', '11.12.2019')
+channels = ('ct1', 'ct2', 'ct24', 'ct4', 'ct5', 'ct6')
+url_first = 'https://www.ceskatelevize.cz/services-old/programme/xml/schedule.php?user=test&date='
+url_last = '&channel='
 
-datum = root.attrib.get('datum_vysilani')
-kanal = root.attrib.get('kanal')
+seznam_url = []
+for channel in channels:
+    for date in dates:
+        url = url_first + date + url_last + channel
+        seznam_url.append(url)
 
 # vyzva uzivateli k zadani filmu a emailu
 seznam_filmu = []
@@ -19,27 +24,36 @@ while True:
         break
     seznam_filmu.append(film)
 
-# vyhledani jednotlivych filmu v programu
+zaslat_email = input('Prejes si zaslat email s vysledkem? a/n ')
+if zaslat_email == 'a':
+    email_uzivatele = input('Zadej svuj email: ')
+
+# vyhledani filmu v programu
 text = list()
-for porad in root:
-  nazev = porad.find('nazvy').find('nazev')
-  for film in seznam_filmu:
-    if film.lower() in nazev.text.lower():
-        cas=porad.find('cas')
-        radek = (datum, kanal, cas.text, nazev.text)
-        text.append(radek)
+for url in seznam_url:
+    response = requests.get(url)
+    response.encoding = 'utf-8'
+    root = ET.fromstring(response.text)
+    datum = root.attrib.get('datum_vysilani')
+    kanal = root.attrib.get('kanal')
+    for porad in root:
+        nazev = porad.find('nazvy').find('nazev')
+        for film in seznam_filmu:
+            if film.lower() in nazev.text.lower():
+                cas = porad.find('cas')
+                radek = (datum, kanal, cas.text, nazev.text)
+                text.append(radek)
 
 
 # notifikacni email s vysledkem
-zaslat_email = input('Prejes si zaslat email s vysledkem? a/n ')
-
 text_emailu = ""
 for radek in text:
     radek_string = "Dne {} na {} v {} dávají {}.\n".format(*radek)
     text_emailu = text_emailu + radek_string
 
+print(text_emailu)
+
 if zaslat_email == 'a':
-    email_uzivatele = input('Zadej svuj email: ')
     sender = 'hlidejfilmy@gmail.com'
     msg = MIMEMultipart()
     msg['From'] = 'hlidejfilmy@gmail.com'
