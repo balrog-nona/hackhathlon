818df668393151173fed70761068fdc289e8403e
petrusakn <44819702+petrusakn@users.noreply.github.com>
2019-12-08 13:11:33 +0100

    I Zpracova

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import requests
 import xml.etree.ElementTree as ET
 response = requests.get('https://www.ceskatelevize.cz/services-old/programme/xml/schedule.php?user=test&date=08.12.2019&channel=ct24')
+response.encoding='utf-8'
 root = ET.fromstring(response.text)
+
+for porad in root:
+  nazev = porad.find('nazvy').find('nazev')
+  cas=porad.find('cas')
+  print(nazev.text, cas.text)
