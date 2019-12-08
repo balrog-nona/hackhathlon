125b15e8de80a2d3398f00b2552278afd5218f3a
petrusakn <44819702+petrusakn@users.noreply.github.com>
2019-12-08 11:23:07 +0100

    XML parser

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-# hlavni soubor k projektu
+import requests
+import xml.etree.ElementTree as ET
+response = requests.get('https://www.ceskatelevize.cz/services-old/programme/xml/schedule.php?user=test&date=08.12.2019&channel=ct24')
+root = ET.fromstring(response.text)
